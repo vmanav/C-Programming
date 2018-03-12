b67f78c1ad0264eb1a6aed7c548ea3c995d6cba4
Manav Verma <32647043+vmanav@users.noreply.github.com>
2018-03-12 19:34:26 +0530

    Swapping of an Array.

---
+++ Arrays/Array_Swap.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{   int A[10],n,i,j,temp,k;
+    printf("\n enter the size");
+    scanf("%d",&n);
+    printf("\n enter array");
+    for(i=0;i<n;i++)
+    {   scanf("%d",&A[i]);
+    }
+    //now swapping
+    if(n%2==0) // EVEN Elements
+    {   k=n-1;
+        for(i=0,j=k;i<=(k/2),j>(k/2);i++,j--)
+        {   temp=A[i];
+            A[i]=A[j];
+            A[j]=temp;
+        }
+    }
+    else // Odd Elements
+    {   k=n-1;
+        for(i=0,j=k;i<(k/2),j>(k/2);i++,j--)
+        {   temp=A[i];
+            A[i]=A[j];
+            A[j]=temp;
+        }
+    }
+
+    for(i=0;i<n;i++)
+    {   printf("\t%d",A[i]);
+    }
+    return 0;
+}
